f3034bdc1e2cbbbccce239511a104b58d1757541
IamYosya <m9y.yosya@gmail.com>
2018-09-11 13:52:52 +0300

    lol

--- cfg.py
+++ cfg.py
@@ -1,6 +1,2 @@
-from time import sleep
-
-while True:
-	token = 'lol'
-	print(token)
-	sleep(2)
+token = '670627833:AAEf5zLRRI71zmW9F_6d2rxh-Wkhx_0y2mA'
+proxy = {'https':'https://66.70.147.196:3128'}
